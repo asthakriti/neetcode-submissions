56838f5baf0d5f31d5e33aeb073861e5e2086423
Astha  Kriti Raj <astha.raj@s.amity.edu>
2026-08-28 08:25:33 +0530

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+
+        vector<vector<int>> adj(numCourses);
+
+        int cnt = 0;
+
+        for(auto it : prerequisites){
+            //it = {u, v}
+            adj[it[1]].push_back(it[0]);
+        }
+        
+        vector<int> indegree(numCourses, 0);
+        //Find indegree
+        for(auto it : adj){
+            //it = {list of array}
+            for(auto node : it){
+                indegree[node]++;
+            }
+        }
+
+        //push indegree = 0 in queue
+
+        queue<int> q;
+          for(int i = 0; i < numCourses; i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+
+        while(!q.empty()){
+
+            int node = q.front();
+            q.pop();
+            cnt++;
+
+            for(auto it : adj[node]){
+                indegree[it]--;
+
+                if(indegree[it] == 0){
+                    q.push(it);
+                }
+            }
+        }
+
+        if(cnt == numCourses){
+            return true;
+        }
+
+        else{
+            return false;
+        }
+
+
+        
+    }
+};
