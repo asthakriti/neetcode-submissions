af93ddbc68b029f8f25bf5bd3474bf72bf74a4c5
Astha  Kriti Raj <astha.raj@s.amity.edu>
2026-09-04 07:16:13 +0530

    Add: median-of-two-sorted-arrays - submission-0

---
+++ Data Structures & Algorithms/median-of-two-sorted-arrays/submission-0.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        
+           // Always binary search on the smaller array
+    if (nums1.size() > nums2.size())
+        return findMedianSortedArrays(nums2, nums1);
+
+    int n = nums1.size();
+    int m = nums2.size();
+
+    int low = 0, high = n;
+
+    while (low <= high) {
+        int cut1 = low + (high - low) / 2;
+        int cut2 = (n + m + 1) / 2 - cut1;
+
+        int left1  = (cut1 == 0) ? INT_MIN : nums1[cut1 - 1];
+        int right1 = (cut1 == n) ? INT_MAX : nums1[cut1];
+
+        int left2  = (cut2 == 0) ? INT_MIN : nums2[cut2 - 1];
+        int right2 = (cut2 == m) ? INT_MAX : nums2[cut2];
+
+        // Correct partition
+        if (left1 <= right2 && left2 <= right1) {
+
+            // Odd total size
+            if ((n + m) % 2 == 1)
+                return max(left1, left2);
+
+            // Even total size
+            return (max(left1, left2) + min(right1, right2)) / 2.0;
+        }
+
+        // nums1 partition is too far right
+        if (left1 > right2)
+            high = cut1 - 1;
+
+        // nums1 partition is too far left
+        else
+            low = cut1 + 1;
+    }
+
+    return -1; // Should never reach here for valid input
+    }
+};
